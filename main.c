2bdac6c1781d29c88e53556e6b420e640abcb31d
bla <bla@bla.com>
2016-01-23 22:41:50 -0500

    Commit

--- main.c
+++ main.c
@@ -1,56 +1,19 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <time.h>
-#include <stdlib.h>
-#include <emmintrin.h>
-#define BUFFER_SIZE 164096
-
-uint32_t tx_time = 500000;
-static volatile __m128i * buffer;
-static volatile __m128i reg;
-
-
-static inline uint32_t current_time() {
-	clock_t now = clock();
-	return now;
-}
-
-static inline void steroids()
-{
-	__m128i* buffer_ptr = (__m128i*) buffer;
-
-	for (uint32_t i = 0 ; i < BUFFER_SIZE ; i++) {
-		_mm_stream_si128(&reg, *buffer_ptr);
-		buffer_ptr = buffer_ptr + 1;
-	}
-}
+#include "transmitter.h"
 
 int main()
 {
-	double period = (double) ((double) (tx_time) / CLOCKS_PER_SEC);
-	double frequency = 1 / period;
-	printf("pulse period = %.6f, pulse frequency = %.6f\n",
-	       period, frequency);
-	buffer = (__m128i*) malloc(sizeof(__m128i) * BUFFER_SIZE);
-	for (uint32_t i = 0 ; i < BUFFER_SIZE ; i++) {
-		buffer[i][0] = rand();
-		buffer[i][1] = rand();
+	uint8_t data[]=  {
+		0b01011010,
+		0b10010001,
+		0b00001000,
+		0b11110001
+	};
+	uint8_t byte = 0b10101010;
+	transmit_init();
+	while (1) {
+		transmit_bytes(&byte, 1);
+		/* transmit_bytes(data, 4); */
 	}
-
-	uint32_t data = 0x55555555;
-
-	for (uint32_t bit_index = 0 ; bit_index < 32 ; bit_index++) {
-		if (((data >> bit_index) & 0b1) == 1) {
-			clock_t begin = clock();
-			while(clock() - begin < tx_time) {
-				steroids();
-			}
-			puts("HIGH");
-		} else {
-			clock_t begin = clock();
-			while(clock() - begin < tx_time);
-			puts("LOW");
-		}
-	}
+	transmit_free();
 }
 
--- transmitter.h
+++ transmitter.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <emmintrin.h>
 
+void transmit_init();
+void transmit_free();
 void transmit_begin();
 void transmit_bytes(uint8_t * bytes, uint8_t len);
 void transmit_end();
